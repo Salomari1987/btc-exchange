6b505a05446dfe881cd4cd2586175419da122e93
Salah Alomari <s.z.alomari.1987@gmail.com>
2020-08-27 00:13:59 +0300

    Dockerize app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # btc-exchange
+
+## SETUP
+* Have Docker installed on your local device
+* Build docker images using compose `docker-compose build`
+* Create a file at the root of the project called local.env and set the API Key and Django Secret Key (you can go to [Djecrety](https://djecrety.ir/)) inside it:
+```env
+SECRET_KEY = {secret_key}
+ALPHA_ADVANTAGE_API_KEY = {apikey}
+```
+* Bring container up using `docker-compose up -d`
+* Run migrations `docker-compose run btcapp python manage.py migrate`
+* Access app at http://localhost:8000
--- btcexchange/settings.py
+++ btcexchange/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+ALPHA_ADVANTAGE_API_KEY = os.environ.get('ALPHA_ADVANTAGE_API_KEY', None)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r0rn4m9%(#pcpyp7htmw_j%952*fu%$fya#dt*ex@r+9y&7qjk'
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
